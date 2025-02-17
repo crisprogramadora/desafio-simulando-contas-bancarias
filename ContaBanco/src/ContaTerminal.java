23fc553c6cb16f65b26ac1603b6fa098dcff554a
crisprogramadora <166080341+crisprogramadora@users.noreply.github.com>
2025-02-17 17:41:55 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,12 +1,3 @@
-//TODO: conhecer e importar a classe Scanner
-
-//Exibir as mensagens para o usuário
-
-//Obter pela scanner os valores digitados no terminal
-
-//Exibir a mensagem conta criada
-
-//link do repositório do desafio: https://github.com/digitalinnovationone/trilha-java-basico/tree/main/desafios/sintaxe
 import java.util.Locale;
 import java.util.Scanner;
 public class ContaTerminal {
